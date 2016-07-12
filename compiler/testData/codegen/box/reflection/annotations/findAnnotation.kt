37a29031cd851e4edf4c65bb3f8a010704c42052
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-07-12 19:54:30 +0300

    Reflection: add KAnnotatedElement.findAnnotation
     #KT-12250 Fixed

---
+++ compiler/testData/codegen/box/reflection/annotations/findAnnotation.kt
@@ -0,0 +1,14 @@
+// WITH_REFLECT
+
+import kotlin.reflect.findAnnotation
+
+annotation class Yes(val value: String)
+annotation class No(val value: String)
+
+@Yes("OK")
+@No("Fail")
+class Foo
+
+fun box(): String {
+    return Foo::class.findAnnotation<Yes>().single().value
+}
